8b30a55e719ceac7a16d9ac7c97744f3cecacef6
PatrickNv23 <patricknv230101@gmail.com>
2024-11-04 17:58:36 -0500

    [feat]: new CV, project and experience.

--- src/constants.ts
+++ src/constants.ts
@@ -12,6 +12,7 @@ import mysqlLogo from './assets/skills_icons/mysql.svg'
 import tailwindLogo from './assets/skills_icons/tailwindcss.svg'
 import foodiusScreenshot from './assets/screenshots/foodius_screenshot.webp';
 import windbnbScreenshot from './assets/screenshots/windbnb_screenshot.webp';
+import kitchenDisplaySystemScreenshot from './assets/screenshots/kitchendisplaysystem_screenshot.webp';
 import todoappScreenshot from './assets/screenshots/todoapp_screenshot.webp';
 import randomquoteScreenshot from './assets/screenshots/randomquote_screenshot.webp';
 import { Skill, ProjectData, ExperienceData, optionNavigation } from './types';
@@ -107,6 +108,21 @@ export const PROJECTS_DATA: Array<ProjectData> = [
     publicUrl: 'https://patricknv23windbnb.netlify.app'
   },
   {
+    imageUrl: kitchenDisplaySystemScreenshot,
+    title: 'Kitchen Display System',
+    description: 'The Kitchen Display system shows and organizes orders on-screen, allowing filtering and prioritization for efficient kitchen management.',
+    technologies: [
+      'React',
+      'Git',
+      'Github',
+      'CSS Modules',
+      'Redux Toolkit',
+      'Styled Components'
+    ],
+    githubUrl: 'https://github.com/PatrickNv23/kitchen-display-system-challenge',
+    publicUrl: 'https://patricknv23kitchendisplaysystem.netlify.app/'
+  },
+  {
     imageUrl: todoappScreenshot,
     title: 'TodoApp',
     description: 'A frontend challenge from the devChallenges page with filters for total, pending and completed tasks, and also contains a save in localStorage.',
@@ -146,6 +162,16 @@ export const EXPERIENCES: Array<ExperienceData> = [
       "Code refactoring applying good practices.",
       "Support for existing functionalities.",
       "Continuous training."
+    ]
+  },
+  {
+    position: "Full Stack Developer",
+    company: "Freelancer",
+    dateRange: "March, June and August 2024",
+    responsabilities: [
+      "I implemented improvements and new requirements that optimized flows by 15%, increasing user satisfaction.",
+      "I replaced 40% of obsolete dependencies, reducing security risks by 25% and failures by 20%.",
+      "I refactored 30% of the code, improving execution times by 18% and reducing resource consumption by 10%.",
     ]
   },
   {
